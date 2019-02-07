5db53554b7de16ece45f182f04fe11bd141fcec4
griernm <griernm@rose-hulman.edu>
2019-02-07 10:23:46 -0500

    finished gui code on m2 for laptop

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -44,17 +44,18 @@ def main():
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
 
-
+    teleop_frame, arm_fram, control_frame = get_shared_frames(main_frame, mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
-    # TODO: Implement and call get_my_frames(...)
+    # DONE: Implement and call get_my_frames(...)
 
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
 
+    grid_frames(teleop_frame, arm_fram, control_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -64,11 +65,18 @@ def main():
 
 
 def get_shared_frames(main_frame, mqtt_sender):
-    pass
 
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
+    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+
+    return teleop_frame, arm_frame, control_frame
 
 def grid_frames(teleop_frame, arm_frame, control_frame):
-    pass
+
+    teleop_frame.grid(row=0, column=0)
+    arm_frame.grid(row=1, column=0)
+    control_frame.grid(row=2, column=0)
 
 
 # -----------------------------------------------------------------------------
