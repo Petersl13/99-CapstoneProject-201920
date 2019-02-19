c2fe492fbc6c33f25945ddfe3e67de7fe68ea854
griernm <griernm@rose-hulman.edu>
2019-02-19 13:30:11 -0500

    some shared gui delegate changes for sprint 3

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -143,3 +143,18 @@ class Handler(object):
         print('got spin then straight at speed', speed)
         m1_extra.spin_then_straight(self.robot, int(speed), int(area))
 
+    def sprint_3(self, speed):
+        print('Got sprint 3 from m2', speed)
+        m2_sprint_3.sprint3(self, int(speed))
+
+    def bark_m2(self):
+        print('Got bark from m2')
+        m2_sprint_3.bark(self)
+
+    def trick_1_m2(self, speed):
+        print('Got trick 1 from m2')
+        m2_sprint_3.trick_1(self, int(speed))
+
+    def trick_2_m2(self, speed):
+        print('Got trick 2 from m2')
+        m2_sprint_3.trick_2(self, int(speed))
